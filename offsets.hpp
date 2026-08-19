21b647da381f9e1edfbe54502c23b75751918dfc
8z8f <253193591+8z8f@users.noreply.github.com>
2026-08-20 10:55:40 +1200

    updated offsets to newest commit

--- offsets.hpp
+++ offsets.hpp
@@ -1,6 +1,6 @@
-// updated: skibidi
-// roblox: 1.0.0
-// sober: 1.0.0
+// updated: 8/18/2026
+// roblox: 2.734.917
+// sober: 1.7.1
 #pragma once
 #include <cstdint>
 
@@ -8,31 +8,31 @@ namespace offsets {
 
     namespace Atmosphere {
         constexpr std::uintptr_t Color = 0xB8;
-        constexpr std::uintptr_t Decay = 0x20;
-        constexpr std::uintptr_t Density = 0x38;
-        constexpr std::uintptr_t Glare = 0x24;
-        constexpr std::uintptr_t Haze = 0x3C;
-        constexpr std::uintptr_t Offset = 0x40;
+        constexpr std::uintptr_t Decay = 0x8;
+        constexpr std::uintptr_t Density = 0x20;
+        constexpr std::uintptr_t Glare = 0x10;
+        constexpr std::uintptr_t Haze = 0x24;
+        constexpr std::uintptr_t Offset = 0x28;
     }
 
     namespace Attachment {
-        constexpr std::uintptr_t Axis = 0xB0;
+        constexpr std::uintptr_t Axis = 0xB4;
         constexpr std::uintptr_t Visible = 0x30;
     }
 
     namespace BasePart {
-        constexpr std::uintptr_t Primitive = 0x118;
+        constexpr std::uintptr_t Primitive = 0x178;
         constexpr std::uintptr_t Reflectance = 0x20;
         constexpr std::uintptr_t Transparency = 0x24;
         constexpr std::uintptr_t Shape = 0x30;
+        constexpr std::uintptr_t Material = 0x34;
         constexpr std::uintptr_t CastShadow = 0xCC;
         constexpr std::uintptr_t Locked = 0xC8;
         constexpr std::uintptr_t Massless = 0xC0;
-        constexpr std::uintptr_t Size = 0x14;
-        constexpr std::uintptr_t Material = 0x84;
         constexpr std::uintptr_t Anchored = 0x1B0;
-        constexpr std::uintptr_t CanQuery = 0x1B1;
-        constexpr std::uintptr_t CanTouch = 0x1B2;
+        constexpr std::uintptr_t CanCollide = 0x2C9;
+        constexpr std::uintptr_t CanQuery = 0x19E;
+        constexpr std::uintptr_t CanTouch = 0x19F;
     }
 
     namespace BloomEffect {
@@ -44,31 +44,31 @@ namespace offsets {
         constexpr std::uintptr_t Position = 0xF4;
         constexpr std::uintptr_t Rotation = 0xD0;
         constexpr std::uintptr_t CameraSubject = 0xC0;
-        constexpr std::uintptr_t ViewportSize = 0x2C0;
+        constexpr std::uintptr_t ViewportSize = 0x2C4;
         constexpr std::uintptr_t CameraType = 0;
     }
 
     namespace DataModel {
-        constexpr std::uintptr_t CreatorId = 0x198;
-        constexpr std::uintptr_t GameId = 0x1A0;
-        constexpr std::uintptr_t JobId = 0x130;
-        constexpr std::uintptr_t PlaceId = 0x170;
+        constexpr std::uintptr_t CreatorId = 0x118;
+        constexpr std::uintptr_t GameId = 0x168;
+        constexpr std::uintptr_t JobId = 0x128;
+        constexpr std::uintptr_t PlaceId = 0x90;
         constexpr std::uintptr_t ServerIp = 0x608;
-        constexpr std::uintptr_t Workspace = 0x150;
+        constexpr std::uintptr_t Workspace = 0x148;
         constexpr std::uintptr_t UniverseId = 0xD8;
-        constexpr std::uintptr_t PrivateServerOwnerId = 0x318;
-        constexpr std::uintptr_t SavaVersion = 0x17C;
+        constexpr std::uintptr_t PrivateServerOwnerId = 0x310;
+        constexpr std::uintptr_t SavaVersion = 0x174;
     }
 
     namespace FakeDataModel {
-        constexpr std::uintptr_t RealDataModel = 0x1D0;
-        constexpr std::uintptr_t Pointer = 0x6DD15A0;
+        constexpr std::uintptr_t RealDataModel = 0x1D8;
+        constexpr std::uintptr_t Pointer = 0x7213FE8;
     }
 
     namespace Humanoid {
         constexpr std::uintptr_t WalkSpeed = 0x1C0;
         constexpr std::uintptr_t WalkSpeedCheck = 0x3AC;
-        constexpr std::uintptr_t Health = 0x17C;
+        constexpr std::uintptr_t Health = 0x180;
         constexpr std::uintptr_t MaxHealth = 0x178;
         constexpr std::uintptr_t HipHeight = 0x184;
         constexpr std::uintptr_t JumpHeight = 0x190;
@@ -77,26 +77,27 @@ namespace offsets {
         constexpr std::uintptr_t RigType = 0x24;
         constexpr std::uintptr_t AutoRotate = 0x1C4;
         constexpr std::uintptr_t SeatPart = 0x270;
-        constexpr std::uintptr_t Sit = 0x2AC;
-        constexpr std::uintptr_t PlatformStand = 0x2AD;
+        constexpr std::uintptr_t Sit = 0x1CE
+        constexpr std::uintptr_t PlatformStand = 0;
         constexpr std::uintptr_t DisplayDistanceType = 0x30;
         constexpr std::uintptr_t NameOcclusion = 0x4C;
-        constexpr std::uintptr_t CameraOffset = 0x20;
-        constexpr std::uintptr_t FloorMaterial = 0x59;
+        constexpr std::uintptr_t CameraOffset = 0x40;
+        constexpr std::uintptr_t FloorMaterial = 0x279;
     }
 
     namespace Instance {
         constexpr std::uintptr_t ChildrenStride = 0x10;
         constexpr std::uintptr_t ClassDescriptor = 0x18;
         constexpr std::uintptr_t ClassName = 0x8;
-        constexpr std::uintptr_t Name = 0x98;
+        constexpr std::uintptr_t Name = 0x70;
+        constexpr std::uintptr_t NameStringOffset = 0x8;
         constexpr std::uintptr_t Parent = 0x68;
-        constexpr std::uintptr_t ChildrenStart = 0x70;
+        constexpr std::uintptr_t ChildrenStart = 0x78;
         constexpr std::uintptr_t ChildrenEnd = 0x8;
     }
 
     namespace Lighting {
-        constexpr std::uintptr_t Brightness = 0xD8;
+        constexpr std::uintptr_t Brightness = 0xE0;
     }
 
     namespace MaterialColors {
@@ -151,17 +152,17 @@ namespace offsets {
     }
 
     namespace Primitive {
-        constexpr std::uintptr_t AssemblyLinearVelocity  = 0xC0;
-        // Manual patch 
-        constexpr std::uintptr_t AssemblyAngularVelocity = 0xCC;
+        constexpr std::uintptr_t AssemblyLinearVelocity  = 0x8C;
+        constexpr std::uintptr_t AssemblyAngularVelocity = 0xB4;
+        constexpr std::uintptr_t AssemblyRotVelocity     = 0xE4;
         constexpr std::uintptr_t CFrame = 0xC8;
-        constexpr std::uintptr_t Material = 0x88;
+        constexpr std::uintptr_t Material = 0xC0;
         constexpr std::uintptr_t Orientation = 0xC8;
         constexpr std::uintptr_t Position = 0xEC;
         constexpr std::uintptr_t PrimitiveFlags = 0x113;
         constexpr std::uintptr_t Rotation = 0xC8;
-        constexpr std::uintptr_t AssemblyRotVelocity = 0xD4;
-        constexpr std::uintptr_t Mass = 0xD8;
+        constexpr std::uintptr_t Mass = 0x1C0;
+        constexpr std::uintptr_t Size = 0x1B8;
         constexpr std::uintptr_t LinearVelocity  = AssemblyLinearVelocity;
         constexpr std::uintptr_t AngularVelocity = AssemblyAngularVelocity;
     }
@@ -174,7 +175,7 @@ namespace offsets {
     }
 
     namespace Print {
-        constexpr std::uintptr_t Print = 0x3EB8C60;
+        constexpr std::uintptr_t Print = 0x3EB801B;
     }
 
     namespace RenderView {
@@ -183,7 +184,6 @@ namespace offsets {
     }
 
     namespace RunService {
-        // Manual patch
         constexpr std::uintptr_t HeartbeatDelta = 0x1D0;
     }
 
@@ -216,64 +216,63 @@ namespace offsets {
     }
 
     namespace UserInputService {
-        constexpr std::uintptr_t MouseEnabled = 0x8B;
-        constexpr std::uintptr_t KeyboardEnabled = 0xB8;
-        constexpr std::uintptr_t TouchEnabled = 0xEF;
+        constexpr std::uintptr_t MouseEnabled = 0x71;
+        constexpr std::uintptr_t KeyboardEnabled = 0x93;
+        constexpr std::uintptr_t TouchEnabled = 0xB8;
     }
 
     namespace VisualEngine {
-        constexpr std::uintptr_t FakeDataModel = 0xA80;
-        constexpr std::uintptr_t Pointer = 0x72C7AF8;
-        constexpr std::uintptr_t RenderView = 0xBA8;
-        constexpr std::uintptr_t ViewMatrix = 0x140;
+        constexpr std::uintptr_t FakeDataModel = 0xAC0;
+        constexpr std::uintptr_t Pointer = 0x79C94F0;
+        constexpr std::uintptr_t RenderView = 0xC00;
+        constexpr std::uintptr_t ViewMatrix = 0x180;
     }
 
     namespace Workspace {
-        constexpr std::uintptr_t CurrentCamera = 0x438;
+        constexpr std::uintptr_t CurrentCamera = 0x450;
         constexpr std::uintptr_t World = 0x3C0;
     }
 
     namespace World {
-        constexpr std::uintptr_t Gravity = 0x208;
         constexpr std::uintptr_t Primitives = 0x400;
     }
 
 } // namespace offsets
 
-// runtime pointers aslr randomized 
+//Runtime service pointers ASLR randomized per run
 namespace services {
-    constexpr std::uintptr_t UserInputService = 0x7FE0A7DD2480;
-    constexpr std::uintptr_t RunService = 0x7FE09C038A80;
-    constexpr std::uintptr_t ReplicatedStorage = 0x7FE0ACA72CC0;
-    constexpr std::uintptr_t ScriptContext = 0x7FE0AF597880;
-    constexpr std::uintptr_t ReplicatedFirst = 0x7FE0A8E1B200;
-    constexpr std::uintptr_t HttpService = 0x7FE09C046D00;
-    constexpr std::uintptr_t LogService = 0x7FE0A8705800;
-    constexpr std::uintptr_t InsertService = 0x7FE0A78CAC00;
-    constexpr std::uintptr_t ContentProvider = 0x7FE0AC3B0080;
-    constexpr std::uintptr_t CollectionService = 0x7FE0A7E4B180;
-    constexpr std::uintptr_t PhysicsService = 0x7FE0B0667880;
-    constexpr std::uintptr_t JointsService = 0x7FE0AC3921C0;
-    constexpr std::uintptr_t TweenService = 0x7FE0B0640C80;
-    constexpr std::uintptr_t Selection = 0x7FE09BC6F600;
-    constexpr std::uintptr_t GuiService = 0x7FE0A61C3F80;
-    constexpr std::uintptr_t Teams = 0x7FE0B54F53A0;
-    constexpr std::uintptr_t BadgeService = 0x7FE0AC392380;
-    constexpr std::uintptr_t SocialService = 0x7FE0B638F580;
-    constexpr std::uintptr_t MarketplaceService = 0x7FE09C256280;
-    constexpr std::uintptr_t TeleportService = 0x7FE0A61C1C80;
-    constexpr std::uintptr_t LocalizationService = 0x7FE09C05E180;
-    constexpr std::uintptr_t PolicyService = 0x7FE0AC391E40;
-    constexpr std::uintptr_t FriendService = 0x7FE0A7E4B480;
-    constexpr std::uintptr_t GroupService = 0x7FE0A7AE2540;
-    constexpr std::uintptr_t PermissionsService = 0x7FE0ACA7E480;
-    constexpr std::uintptr_t RobloxReplicatedStorage = 0x7FE0A7273500;
-    constexpr std::uintptr_t RbxAnalyticsService = 0x7FE0A8E1D700;
-    constexpr std::uintptr_t AnalyticsService = 0x7FE0AC392540;
-    constexpr std::uintptr_t VRService = 0x7FE09C05EB80;
-    constexpr std::uintptr_t TextChatService = 0x7FE09C0CA280;
-    constexpr std::uintptr_t GamepadService = 0x7FE0B964DB80;
-    constexpr std::uintptr_t KeyframeSequenceProvider = 0x7FE09BB6E780;
-    constexpr std::uintptr_t AnimationClipProvider = 0x7FE0A7E49F80;
-    constexpr std::uintptr_t MaterialService = 0x7FE09C24D280;
+    constexpr std::uintptr_t UserInputService = 0x7F74A0EC1880;
+    constexpr std::uintptr_t RunService = 0x7F74A0C3A580;
+    constexpr std::uintptr_t ReplicatedStorage = 0x7F749D652700;
+    constexpr std::uintptr_t ScriptContext = 0x7F74AA273C80;
+    constexpr std::uintptr_t ReplicatedFirst = 0x7F74A9094100;
+    constexpr std::uintptr_t HttpService = 0x7F74A0C66980;
+    constexpr std::uintptr_t LogService = 0x7F74A076A400;
+    constexpr std::uintptr_t InsertService = 0x7F74A9DFC8E0;
+    constexpr std::uintptr_t ContentProvider = 0x7F74B8CA0080;
+    constexpr std::uintptr_t CollectionService = 0x7F74B56ABD80;
+    constexpr std::uintptr_t PhysicsService = 0x7F74A867E180;
+    constexpr std::uintptr_t JointsService = 0x7F74A0C4C640;
+    constexpr std::uintptr_t TweenService = 0x7F74B16B5A80;
+    constexpr std::uintptr_t Selection = 0x7F74B05C1A00;
+    constexpr std::uintptr_t GuiService = 0x7F74AF957E80;
+    constexpr std::uintptr_t Teams = 0x7F749D650940;
+    constexpr std::uintptr_t BadgeService = 0x7F74A0C4C800;
+    constexpr std::uintptr_t SocialService = 0x7F74B8AA7880;
+    constexpr std::uintptr_t MarketplaceService = 0x7F74B1701C80;
+    constexpr std::uintptr_t TeleportService = 0x7F74AF95AF80;
+    constexpr std::uintptr_t LocalizationService = 0x7F74A0CBEB80;
+    constexpr std::uintptr_t PolicyService = 0x7F74A0C4C2C0;
+    constexpr std::uintptr_t FriendService = 0x7F74B56AC080;
+    constexpr std::uintptr_t GroupService = 0x7F74C0665300;
+    constexpr std::uintptr_t PermissionsService = 0x7F74B1D58900;
+    constexpr std::uintptr_t RobloxReplicatedStorage = 0x7F74B1D07240;
+    constexpr std::uintptr_t RbxAnalyticsService = 0x7F74A9092700;
+    constexpr std::uintptr_t AnalyticsService = 0x7F74A0C4C9C0;
+    constexpr std::uintptr_t VRService = 0x7F74A0CBC880;
+    constexpr std::uintptr_t TextChatService = 0x7F74A0EC7880;
+    constexpr std::uintptr_t GamepadService = 0x7F74AFFB3F80;
+    constexpr std::uintptr_t KeyframeSequenceProvider = 0x7F74A0C45640;
+    constexpr std::uintptr_t AnimationClipProvider = 0x7F74B56AB780;
+    constexpr std::uintptr_t MaterialService = 0x7F74B1702A80;
 } // namespace services
